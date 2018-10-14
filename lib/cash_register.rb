2f3d358b52c2a939cd1ce49cf426f43a6898a234
jameslee1187 <jameslee1187@gmail.com>
2018-10-14 15:22:35 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
-
+class CashRegister
+  attr_accessor :total
+  attr_reader :discount
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  def total
+    @total
+  end
+  def add_item
+  end
+  
+end
